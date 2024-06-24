ae7c0976589888f81335ae618cec4f60c57bf604
Dalembert21 <76698604+Dalembert21@users.noreply.github.com>
2024-06-23 22:13:08 -0500

    pruebas unitarias detalles eventos

--- src/app/client/pages/ad-detail/ad-detail.component.spec.ts
+++ src/app/client/pages/ad-detail/ad-detail.component.spec.ts
@@ -1,16 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AdDetailComponent } from './ad-detail.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NzNotificationModule } from 'ng-zorro-antd/notification';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
-describe('AdDetailComponent', () => {
+fdescribe('AdDetailComponent', () => {
   let component: AdDetailComponent;
   let fixture: ComponentFixture<AdDetailComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AdDetailComponent ]
-    })
-    .compileComponents();
+      declarations: [AdDetailComponent],
+      imports: [
+        HttpClientModule,
+        ReactiveFormsModule,
+        NzNotificationModule,
+        NzButtonModule,
+        NzFormModule,
+        FormsModule,
+        NzDatePickerModule,
+        RouterTestingModule,
+      ],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              params: { adId: '123' }, // Proveer datos mock para snapshot.params
+            },
+            params: of({ adId: '123' }), // También puedes usar esto si necesitas el observable
+          },
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AdDetailComponent);
     component = fixture.componentInstance;
@@ -19,5 +47,38 @@ describe('AdDetailComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display ad details correctly', () => {
+    // Simular valores para guardar
+    component.ad = {
+      serviceName: 'Servicio de Ejemplo',
+      price: 100,
+      description: 'Descripción del servicio',
+      companyName: 'Nombre de la Empresa',
+      // Otros campos necesarios para tus pruebas
+    };
+
+    // Simular valores del formulario
+    component.validateForm.patchValue({
+      bookDate: new Date(), // Aquí puedes simular una fecha u otro valor según sea necesario
+      // Otros campos del formulario si los tienes
+    });
+
+    fixture.detectChanges();
+
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h2').textContent).toContain(
+      component.ad.serviceName
+    );
+    expect(compiled.querySelector('h5:nth-of-type(1)').textContent).toContain(
+      component.ad.price
+    );
+    expect(compiled.querySelector('h5:nth-of-type(2)').textContent).toContain(
+      component.ad.description
+    );
+    expect(compiled.querySelector('h5:nth-of-type(2)').textContent).toContain(
+      component.ad.companyName
+    );
   });
 });
--- src/app/client/pages/ad-detail/ad-detail.component.ts
+++ src/app/client/pages/ad-detail/ad-detail.component.ts
@@ -8,58 +8,53 @@ import { UserStoargeService } from 'src/app/basic/services/storage/user-stoarge.
 @Component({
   selector: 'app-ad-detail',
   templateUrl: './ad-detail.component.html',
-  styleUrls: ['./ad-detail.component.scss']
+  styleUrls: ['./ad-detail.component.scss'],
 })
 export class AdDetailComponent {
+  adId = this.activatedroute.snapshot.params['adId'];
+  avatarUrl: any;
+  ad: any;
 
-  adId= this.activatedroute.snapshot.params['adId'];
-  avatarUrl:any;
-  ad:any;
-
-  reviews:any;
+  reviews: any;
 
   validateForm!: FormGroup;
 
-  constructor(private clientService: ClientService,
+  constructor(
+    private clientService: ClientService,
     private activatedroute: ActivatedRoute,
     private notification: NzNotificationService,
     private router: Router,
-    private fb: FormBuilder){}
+    private fb: FormBuilder
+  ) {}
 
+  ngOnInit() {
+    this.validateForm = this.fb.group({
+      bookDate: [null, [Validators.required]],
+    });
+    this.getAdDetailsByAdId();
+  }
 
-    ngOnInit(){
-      this.validateForm = this.fb.group({
-        bookDate: [null, [Validators.required]]
-      })
-      this.getAdDetailsByAdId();
-    }
+  getAdDetailsByAdId() {
+    this.clientService.getAdDetailsByAdId(this.adId).subscribe((res) => {
+      console.log(res);
+      this.avatarUrl = 'data:image/jpeg;base64,' + res.adDTO.returnedImg;
+      this.ad = res.adDTO;
+      this.reviews = res.reviewDTOList;
+    });
+  }
 
+  bookService() {
+    const bookServiceDTO = {
+      bookDate: this.validateForm.get(['bookDate']).value,
+      adId: this.adId,
+      userId: UserStoargeService.getUserId(),
+    };
 
-    getAdDetailsByAdId(){
-      this.clientService.getAdDetailsByAdId(this.adId).subscribe(res=>{
-        console.log(res);
-        this.avatarUrl = 'data:image/jpeg;base64,' + res.adDTO.returnedImg;
-        this.ad = res.adDTO;
-        this.reviews = res.reviewDTOList;
-      })
-    }
-
-    bookService(){
-      const bookServiceDTO = {
-        bookDate : this.validateForm.get(['bookDate']).value,
-        adId : this.adId,
-        userId: UserStoargeService.getUserId()
-      }
-
-      this.clientService.bookService(bookServiceDTO).subscribe(res =>{
-        this.notification
-        .success(
-          'SUCCESS',
-          `Se añadio correctamente!`,
-          { nzDuration: 5000 }
-        );
-        this.router.navigateByUrl('/client/bookings');
-      })
-    }
-
+    this.clientService.bookService(bookServiceDTO).subscribe((res) => {
+      this.notification.success('SUCCESS', `Se añadio correctamente!`, {
+        nzDuration: 5000,
+      });
+      this.router.navigateByUrl('/client/bookings');
+    });
+  }
 }
