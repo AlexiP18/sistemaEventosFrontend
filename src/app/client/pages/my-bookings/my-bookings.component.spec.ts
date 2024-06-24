dc168cfcb2163e82b7e885aec775969bdd3adcf1
Dalembert21 <76698604+Dalembert21@users.noreply.github.com>
2024-06-23 22:23:01 -0500

    prueba de crear el modulo del panel del cliente

--- src/app/client/pages/my-bookings/my-bookings.component.spec.ts
+++ src/app/client/pages/my-bookings/my-bookings.component.spec.ts
@@ -1,16 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MyBookingsComponent } from './my-bookings.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NzTableModule } from 'ng-zorro-antd/table'; // Importa el módulo correspondiente
 
-describe('MyBookingsComponent', () => {
+fdescribe('MyBookingsComponent', () => {
   let component: MyBookingsComponent;
   let fixture: ComponentFixture<MyBookingsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MyBookingsComponent ]
-    })
-    .compileComponents();
+      declarations: [MyBookingsComponent],
+      imports: [
+        HttpClientModule,
+        NzTableModule, // Agrega NzTableModule aquí
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(MyBookingsComponent);
     component = fixture.componentInstance;
