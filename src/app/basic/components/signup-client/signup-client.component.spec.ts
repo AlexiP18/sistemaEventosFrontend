1da74d55bf90238ffc1d91091bed33acc71738c4
Dalembert21 <76698604+Dalembert21@users.noreply.github.com>
2024-06-23 21:43:30 -0500

    pruebas de registro en el cliente

--- src/app/basic/components/signup-client/signup-client.component.spec.ts
+++ src/app/basic/components/signup-client/signup-client.component.spec.ts
@@ -1,16 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignupClientComponent } from './signup-client.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NzNotificationModule } from 'ng-zorro-antd/notification';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import {
+  NzFormControlComponent,
+  NzFormItemComponent,
+} from 'ng-zorro-antd/form';
 
-describe('SignupClientComponent', () => {
+fdescribe('SignupClientComponent', () => {
   let component: SignupClientComponent;
   let fixture: ComponentFixture<SignupClientComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SignupClientComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        SignupClientComponent,
+        NzFormItemComponent,
+        NzFormControlComponent,
+      ],
+      imports: [
+        HttpClientModule,
+        ReactiveFormsModule,
+        NzNotificationModule,
+        NzButtonModule, // Importa NzButtonModule aquí
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SignupClientComponent);
     component = fixture.componentInstance;
@@ -19,5 +36,25 @@ describe('SignupClientComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should create SignupClientComponent', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should initialize the form with correct fields and validators', () => {
+    expect(component.validateForm).toBeDefined();
+    expect(component.validateForm.get('email')).toBeTruthy();
+    expect(component.validateForm.get('password')).toBeTruthy();
+    expect(component.validateForm.get('checkPassword')).toBeTruthy();
+    expect(component.validateForm.get('name')).toBeTruthy();
+    expect(component.validateForm.get('lastname')).toBeTruthy();
+    expect(component.validateForm.get('phone')).toBeTruthy();
+
+    // Test Validators
+    const emailControl = component.validateForm.get('email');
+    emailControl.setValue('invalidemail'); // Set invalid email format
+    expect(emailControl.valid).toBeFalsy();
+    emailControl.setValue('valid@email.com'); // Set valid email format
+    expect(emailControl.valid).toBeTruthy();
   });
 });
