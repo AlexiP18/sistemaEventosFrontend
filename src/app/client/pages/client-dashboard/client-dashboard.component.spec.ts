91c9061dd4366403b9f1847c4e7a6ba9421faa5c
Dalembert21 <76698604+Dalembert21@users.noreply.github.com>
2024-06-23 22:19:27 -0500

    prueba unitaria creacion del modulo cliente/dashboard

--- src/app/client/pages/client-dashboard/client-dashboard.component.spec.ts
+++ src/app/client/pages/client-dashboard/client-dashboard.component.spec.ts
@@ -1,16 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ClientDashboardComponent } from './client-dashboard.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms'; // Importa el módulo correcto
 
-describe('ClientDashboardComponent', () => {
+fdescribe('ClientDashboardComponent', () => {
   let component: ClientDashboardComponent;
   let fixture: ComponentFixture<ClientDashboardComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ClientDashboardComponent ]
-    })
-    .compileComponents();
+      declarations: [ClientDashboardComponent],
+      imports: [
+        HttpClientModule,
+        ReactiveFormsModule, // Agrega el módulo de formularios reactivos
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ClientDashboardComponent);
     component = fixture.componentInstance;
